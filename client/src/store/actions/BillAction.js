eae3b180e1f1f4f6ff64669fec46292400b36919
PedrazaDiego <skapet01@gmail.com>
2022-01-14 14:22:03 -0500

    baseline client setup

---
+++ client/src/store/actions/BillAction.js
@@ -0,0 +1,16 @@
+import { GetBills } from "../../services/BillServices";
+import { GET_BILLS } from "../types";
+
+export const LoadBills = () => {
+    return async (dispatch) => {
+        try {
+            const bills = await GetBills()
+            dispatch({
+                type: GET_BILLS,
+                payload: bills
+            })
+        } catch (error) {
+            throw error
+        }
+    }
+}
