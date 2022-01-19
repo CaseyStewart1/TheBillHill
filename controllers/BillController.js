865f1d5a8e6692fc563433d3b32e9b22aea8314e
Jonathan <jmast1913@gmail.com>
2022-01-19 10:14:12 -0500

    Fake code

--- controllers/BillController.js
+++ controllers/BillController.js
@@ -1,14 +1,16 @@
-const { Bill } = require('../models')
+const { Bill } = require('../models');
 
 const GetAllBills = async (req, res) => {
-    try {
-        const response = await Bill.findAll()
-        res.send(response)
-    } catch (error) {
-        throw error
-    }
-}
+  try {
+    const response = await Bill.findAll();
+    res.send(response);
+  } catch (error) {
+    throw error;
+  }
+};
 
 module.exports = {
-    GetAllBills
-}
+  GetAllBills
+};
+
+// FAKE CODE!!!!!
