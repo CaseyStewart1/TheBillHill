fa96fe1fc2fb2829c11cd3b1507d9731febce45e
CaseyStewart1 <caseystewartdesigns@mail.com>
2022-01-20 15:45:07 -0500

    express added to serer.js

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
-const app = require('express')()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const path = require('path')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
 
-const AppRouter = require('./routes/AppRouter')
+const AppRouter = require('./routes/AppRouter');
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.json({ message: 'Server Works' }))
+app.get('/', (req, res) => res.json({ message: 'Server Works' }));
 
-app.use('/api', AppRouter)
+app.use('/api', AppRouter);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')))
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(`${__dirname}/client/build/index.html`))
-    })
-  }
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/client/build/index.html`));
+  });
+}
 
-app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
+app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`));
